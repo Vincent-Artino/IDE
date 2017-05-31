cb262d282e866b0ba880c262126cafca901cf2f0
Vincent Artino <Vincent-Artino@users.noreply.github.com>
2017-05-31 15:46:58 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,9 @@ var app = express()
 var port = Number(process.env.PORT||5000)
 app.use(express.static(path.join(__dirname)));
 app.get('/',function(req,res){
-res.sendFile(path.join(__dirname+'/../views/front.html'));
-res.sendFile(path.join(__dirname+'/../views/theme.html'));
-  
+res.sendFile(path.join(__dirname+'/../views/front.html'));  
+})
+app.get('/theme.css',function(req,res){
+res.sendFile(path.join(__dirname+'/../views/theme.css'));  
 })
 app.listen(port)
