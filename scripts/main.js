975b4c9bdda82e321eb964b7e02d2024acbda1d5
Vincent Artino <Vincent-Artino@users.noreply.github.com>
2017-05-31 15:11:05 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,9 @@ var request = require('request')
 var bodyParser = require('body-parser')
 var path = require('path')
 var app = express()
+var port = Number(process.env.PORT||5000)
 app.use(express.static(path.join(__dirname)));
 app.get('/',function(req,res){
 res.sendFile(path.join(__dirname+'/views/front.html'));
 })
+app.listen(port)
