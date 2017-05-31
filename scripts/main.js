1261014ff9383e29b6a5b9d9288e3ec4d13dd6c6
Vincent Artino <Vincent-Artino@users.noreply.github.com>
2017-05-31 15:04:23 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
-
+var express = require('express')
+var http = require('http')
+var request = require('request')
+var bodyParser = require('body-parser')
+var path = require('path')
+var app = express()
+app.use(express.static(path.join(__dirname)));
+app.get('/',function(req,res){
+res.sendFile(path.join(__dirname+'/views/index.html'));
+})
